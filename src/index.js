4860b605153e5f2b03b49916ad8b82089eab8238
Rummut <kzantip@gmail.com>
2023-08-29 13:17:02 +0300

    add edits

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { App } from 'components/App.hook';
+import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
